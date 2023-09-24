fe51e86b7eedd5f4aff667d2120fe5d0c2b7fa63
Mariia Khraban <khrabanm@yandex.ru>
2023-09-24 16:11:59 +0300

    add dotenv variables

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../utils/constants');
+const { DEV_SECRET } = require('../utils/constants');
 const ErrorAccess = require('../utils/errors/ErrorAccess');
+
+const { JWT_SECRET, NODE_ENV } = process.env;
+
 
 const handleAuthError = (req, res, next) => next(new ErrorAccess('Необходима авторизация'));
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const { token } = req.cookies;
+  const { authorization } = req.headers;
+  const token = authorization.replace('Bearer ', '');
   try {
     if (!token) {
       return handleAuthError(req, res, next);
     }
-    const payload = jwt.verify(token, JWT_SECRET);
+    const payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : DEV_SECRET);
     req.user = payload;
     next();
   } catch (err) {
